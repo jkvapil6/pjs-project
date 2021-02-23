ceed7f8aab1ff353683461f65f0b31c97a505687
unknown <jkvapil6@gmail.com>
2021-02-23 10:38:01 +0100

    remote db conn heroku

--- pages/api/emp.js
+++ pages/api/emp.js
@@ -2,16 +2,45 @@
 
 export default (req, res) => {
   
+  // const knex = require('knex')({
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: "./db.db"
+  //   }
+  // })
+  // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  
+  // const conn = {
+  //   host : 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com',
+  //   user : 'pcutuxsqwymfiy',
+  //   password : '769ea73d75fae6473c688023df31f238a0fae74d163b29e73453c03d907f1687',
+  //   database : 'dbc9cqbl35v478',
+  //   ssl: true,
+  //   insecureAuth: true
+  // }
+
+    
+  const conn = {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    ssl: true,
+    insecureAuth: true
+  }
+
+
   const knex = require('knex')({
-    client: 'sqlite3',
-    connection: {
-      filename: "./db.db"
-    }
+    client: 'pg',
+    version: '7.2',
+    connection: conn
   })
 
   const getEmployees = async () => {
-    const emps = await knex('employee')
+    const emps = await knex.withSchema('public')
       .select("*")
+      .from("Employee")
+
     console.log(emps)
     knex.destroy()
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
+
 import Content from '../components/common/Content'
 
 export default function Home() {
+
+  const [emps, setEmps] = useState([])
+
+  useEffect(() => {
+    getEmployees()
+  }, [])
+
+  const getEmployees = async () => {
+    const res = await fetch('/api/emp')
+    const data = await res.json()
+
+    if (data.emps) {
+      console.log(data.emps)
+      setEmps(data.emps)
+    }
+  }
+
   return (
     <Content>
-      <h1>Hello world!</h1>
+      <h1>Employees</h1>
+      <ul>
+        { emps.map(e => (
+          <li key={e.id}>{e.name}</li>
+        )) }
+      </ul>
     </Content>
   )
 }
